ee347f761f28913074bbcc6acc7064c79f3bd2ce
de-ut <balabanov_aleksei@mail.ru>
2019-09-07 03:48:54 +0300

    added finishing off ships

--- sb/ai.js
+++ sb/ai.js
@@ -29,6 +29,13 @@ function AIAtack(){
   AIFind();
   console.log(x + " " + y + " " + matrix1[x][y]);
   if(matrix1[x][y] == 's'){
+    if(mode == 0){
+      shipx = x;
+      shipy = y;
+      mode = 1;
+      rotation = 0;
+      direct = 1;
+    }
     map[x][y] = 2;
   }else{
     map[x][y] = 1;
@@ -77,6 +84,87 @@ let AIFind = () => {
       }
     }
   }else{
+    let kill = isKilled(shipx, shipy);
+    if(kill != -1){
+      console.log("FINISHED");
+      console.log(map);
+      count[kill]--;
+      mode = 0;
+      x = shipx;
+      y = shipy;
+      AIFind();
+    }else{
+      console.log(rotation +" " + direct);
+      if(map[x][y] == 1) changeDirect();
+      if(rotation == 0){
+        x+=direct;
+        if(x > 9 || x < 0 || map[x][y] != -1){
+          changeDirect();
+        }
+      } else{
+        y+=direct;
+        if(y > 9 || y < 0 || map[x][y] != -1){
+          changeDirect();
+        }
+      }
+    }
   }
   if(x > 9 || x < 0 || y > 9 || y < 0 || map[x][y] != -1) AIFind();
+}
+
+let changeDirect = () => {
+  x = shipx;
+  y = shipy;
+  if(rotation == 0){
+    if(direct == 1) direct = -1;
+    else{
+      rotation = 1;
+      direct = 1;
+    }
+  }else{
+    if(direct == 1) direct = -1;
+    else console.log("ERROR! CANT FINISH SHIP");
+  }
+}
+
+let isKilled = (x, y) => {
+  console.log("check");
+  if(matrix1[x][y] == 's' && map[x][y] != 2) return -1;
+  if((x+1 < 10 && matrix1[x+1][y] == 's') || (x-1 >= 0 && matrix1[x-1][y] == 's')){
+    let currX = x;
+    while(currX+1 < 10 && matrix1[currX+1][y] == 's'){
+      currX++;
+    }
+    let count = 0
+    while(currX-1 >= 0 && matrix1[currX-1][y] == 's'){
+      if(map[currX][y] != 2){
+        return -1;
+      }
+      currX--;
+      count++;
+    }
+    if(map[currX][y] != 2){
+      return -1;
+    }
+    return count;
+
+  }else if((y+1 < 10 && matrix1[x][y+1] == 's') || (y-1 >= 0 && matrix1[x][y-1] == 's')){
+    let currY = y;
+    while(currY+1 < 10 && matrix1[x][currY+1] == 's'){
+      currY++;
+    }
+    let count = 0;
+    while(currY-1 >= 0 && matrix1[x][currY-1] == 's'){
+      if(map[x][currY] != 2){
+        return -1;
+      }
+      currY--;
+      count++;
+    }
+    if(map[x][currY] != 2){
+      return -1;
+    }
+    
+    return count;
+  }
 }
